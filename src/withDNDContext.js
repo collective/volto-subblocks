71ea37848875d9a8601c159513a5209d6ea8c61e
Giulia Ghisini <giulia.ghisini@redturtle.it>
2022-05-27 16:28:11 +0200

    fix: fix duplicate HTML Backend contextt

--- src/withDNDContext.js
+++ src/withDNDContext.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { injectLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
 
+let dndContextSingleton = null;
 const withDNDContext = (component) => {
   const DNDConnector = (props) => {
     const { DragDropContext } = props.reactDnd;
     const HTML5Backend = props.reactDndHtml5Backend.default;
+    if (!dndContextSingleton) {
+      dndContextSingleton = DragDropContext(HTML5Backend);
+    }
 
-    const DNDSubblocks = React.useMemo(
-      () => DragDropContext(HTML5Backend)(component),
-      [DragDropContext, HTML5Backend],
-    );
+    const DNDSubblocks = React.useMemo(() => dndContextSingleton(component), [
+      DragDropContext,
+      HTML5Backend,
+    ]);
 
     return <DNDSubblocks {...props} />;
   };
