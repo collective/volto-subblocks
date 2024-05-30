da4150d52a7f808433b301ae2d2c945a9f091dbb
Victor Fernandez de Alba <sneridagh@gmail.com>
2024-05-30 19:31:14 +0200

    No prettier

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-export { default as withDNDContext } from "./withDNDContext";
-export { injectDNDSubblocks } from "./DNDSubblocks";
-export { default as SubblocksEdit } from "./SubblocksEdit";
-export { default as SubblockEdit } from "./SubblockEdit";
-export { default as SubblocksWrapper } from "./SubblocksWrapper";
-export { default as Subblock } from "./Subblock";
+export { default as withDNDContext } from './withDNDContext';
+export { injectDNDSubblocks } from './DNDSubblocks';
+export { default as SubblocksEdit } from './SubblocksEdit';
+export { default as SubblockEdit } from './SubblockEdit';
+export { default as SubblocksWrapper } from './SubblocksWrapper';
+export { default as Subblock } from './Subblock';
 
 const applyConfig = (config) => config;
 export default applyConfig;
